f8ce432ffdaa8889f23dd753407892fe0c4d1b82
jiaoz <zhangjiao989@gmail.com>
2016-02-17 18:12:18 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+matrixinverse <- NULL                     
+    ## delcare another function set where the value will be cached in 
+    set <- function(y) {                      
+        x <<- y
+        matrixinverse <<- NULL              
+    }
+    ## gets the value of the inverse
+    get <- function() x                           
+    #calculates the inverse of non-singular matrix via the solve function
+    setinverse <- function(solve) matrixinverse <<- solve 
+    # gets the inverse     
+    getinverse <- function() matrixinverse        
+    ## passes the value of the function makeCacheMatrix        
+    list(set = set, get = get,                    
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), 
+##then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+            matrixinverse <- x$getinverse()
+ #if the inverse exists, it gets it.
+    if(!is.null(matrixinverse)) {                 
+        message("getting cached data - Inverse of the matrix")
+        return(matrixinverse)
+    }
+    #if the inverse if not there, first it is calculated and then retrieved.
+    data <- x$get()                               
+    matrixinverse <- solve(data, ...)
+    x$setinverse(matrixinverse)
+    matrixinverse
 }
